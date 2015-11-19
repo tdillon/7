42bbda6cb0a661682f2eb898b5cc94240fcd60ad
Travis <tdillon25@gmail.com>
2015-11-19 16:25:50 -0500

    creating demo page using jspm

---
+++ config.js
@@ -0,0 +1,12 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "none",
+  paths: {
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "seven-segment": "npm:seven-segment@3.0.1"
+  }
+});
